519cb93c33893073ef7210767a50e4924c3b53c5
krosengr4 <rosenkev4@gmail.com>
2025-06-18 11:15:55 -0500

    Created homepage endpoint to complete exercise 1 on page 112 of workbook 9

---
+++ NorthwindTradersAPI/src/main/java/com/example/NorthwindTradersAPI/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.NorthwindTradersAPI.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+   @GetMapping(path = "/")
+   public String homepage(@RequestParam (defaultValue = "World")
+						  String country) {
+	  return "Hello, " + country + "!";
+   }
+
+}
